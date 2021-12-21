27cee24feb902357f104908f69f8d17f0731032c
Jon Knight <jon@metrixapps.com>
2021-12-21 15:06:49 -0500

    fist code for testing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+module.exports = require('./lib/XanoFactory');
+
+// const xano = require('./lib/XanoFactory');
+// (async () => {
+// 	let ret = await xano.get("https://jsonplaceholder.typicode.com/todos/1")
+// 	console.log("RET", ret);
+// })()
